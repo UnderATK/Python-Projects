b14d8d2de1f24e5d41b5c531635293b13359e864
Dima Shapira <33936831+UnderATK@users.noreply.github.com>
2023-07-05 14:32:13 +0300

    Update main.py

--- CheckersAI/main.py
+++ CheckersAI/main.py
@@ -1,6 +1,4 @@
-###############################
-# Dima Shapira & Yael Shapira #
-###############################
+
 import pygame
 from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED
 from checkers.game import Game
